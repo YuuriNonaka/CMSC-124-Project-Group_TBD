dc7ce3d3988ba4c0f4863e8c7d828dd4d4802830
Fabyss04 <123131407+Fabyss04@users.noreply.github.com>
2025-11-10 21:38:29 +0800

    fix: changed symbolizer logic for IT entry

--- lolcode_interpreter/symbolizer.py
+++ lolcode_interpreter/symbolizer.py
@@ -1,10 +1,25 @@
 from lexer import TokenType
 
+def get_value(tokens, symbol_table):
+    values = []
+    for lexeme, token_type, line_num in tokens:
+        if token_type == TokenType.YARN or token_type == TokenType.NUMBR or token_type == TokenType.NUMBAR or token_type == TokenType.TROOF:
+            values.append(lexeme)
+        
+        elif token_type == TokenType.VARIDENT:
+            values.append(str(symbol_table.get(lexeme, "")))
+        
+        elif token_type == TokenType.STRING_DELIM:
+            pass
+    
+    return " ".join(values)
+
 def symbolize(tokens):
     symbol_table = {}
     symbol_table['IT'] = ""
 
-    for i in range(len(tokens)):
+    i = 0
+    while i < len(tokens):
         lexeme, token_type, line_num = tokens[i]
 
         # I HAS A <var> ITZ <value>
@@ -14,11 +29,17 @@ def symbolize(tokens):
             (tokens[i + 2][1] == TokenType.ITZ)):
 
             var_name = tokens[i+1][0]
-            value = tokens[i+3][0]
+            
+            value_tokens = []
+            index = i + 3
+            while index < len(tokens) and tokens[index][1] != TokenType.LINEBREAK:
+                value_tokens.append(tokens[index])
+                index += 1
 
+            value = get_value(value_tokens, symbol_table)
             symbol_table[var_name] = value
 
-            i += 4
+            i = index
             continue
 
         # I HAS A <var> 
@@ -30,7 +51,27 @@ def symbolize(tokens):
             if var_name not in symbol_table:
                 symbol_table[var_name] = ""
             
-            i += 2
+            index = i + 2
+            while index < len(tokens) and tokens[index][1] != TokenType.LINEBREAK:
+                index += 1
+
+            i += index
             continue
+
+        # VISIBLE
+        elif token_type == TokenType.VISIBLE:
+            value_tokens = []
+            index = i + 1
+            while index < len(tokens) and tokens[index][1] != TokenType.LINEBREAK:
+                value_tokens.append(tokens[index])
+                index += 1
+            
+            value = get_value(value_tokens, symbol_table)
+            symbol_table["IT"] = value
+
+            i = index
+            continue
+            
+        i += 1
     
     return symbol_table
