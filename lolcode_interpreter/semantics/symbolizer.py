e71af9361d247079a6e17986b9ebfa0e4c1d758b
PaulFababeir <pffababeir@up.edu.ph>
2025-11-13 11:19:42 +0800

    feat/refactor: imrpoved getvalue, performs operations

--- lolcode_interpreter/semantics/symbolizer.py
+++ lolcode_interpreter/semantics/symbolizer.py
@@ -1,22 +1,104 @@
 from lexer.lol_tokens import TokenType
+import operator
 
 def get_value(tokens, symbol_table): #helper function: converts expression into a single string value ("noot noot" var => noot noot 12)
-    values = []
-    for lexeme, token_type, line_num in tokens:
-        if token_type == TokenType.YARN or token_type == TokenType.NUMBR or token_type == TokenType.NUMBAR or token_type == TokenType.TROOF:
-            values.append(lexeme) #if simply a literal, just append.
-        
-        elif token_type == TokenType.VARIDENT:
-            values.append(str(symbol_table.get(lexeme, ""))) #if a variable, find its value, turn into a string, and append.
-        
-        elif token_type == TokenType.STRING_DELIM:
-            pass #delimiters are not included
     
-    return " ".join(values)
+    if not tokens:
+        return "NOOB"
+    
+    if len(tokens) == 1:
+        lexeme, token_type, line_num = tokens[0]
+
+        # print(f"{lexeme} - {token_type} - {line_num}")
+
+        if token_type == TokenType.NUMBR:
+            return int(lexeme)
+        if token_type == TokenType.NUMBAR:
+            return float(lexeme)
+        if token_type == TokenType.YARN:
+            return lexeme
+        if token_type == TokenType.TROOF:
+            if lexeme == "WIN":
+                return True
+            else:
+                return False
+        if token_type == TokenType.NOOB:
+            return "NOOB"
+        if token_type == TokenType.VARIDENT:
+            value = symbol_table.get(lexeme, "")
+            try:
+                return int(value)
+            except(ValueError, TypeError):
+                try:
+                    return float(value)
+                except(ValueError, TypeError):
+                    return str(value)
+        else:
+            return lexeme
+
+    if ((len(tokens) == 3) and
+        (tokens[0][1] == TokenType.STRING_DELIM) and
+        (tokens[1][1] == TokenType.YARN) and
+        (tokens[2][1] == TokenType.STRING_DELIM)):
+        return tokens[1][0]
+    
+    operator_lexeme, operator_type, operator_num = tokens[0]
+    operations = {
+        TokenType.SUM_OF: operator.add,
+        TokenType.DIFF_OF: operator.sub,
+        TokenType.PRODUKT_OF: operator.mul,
+        TokenType.QUOSHUNT_OF: operator.truediv,
+        TokenType.MOD_OF: operator.mod,
+        TokenType.BIGGR_OF: max,
+        TokenType.SMALLR_OF: min,
+    }
+
+    if operator_type in operations:
+        AN_index = -1
+        for i, (lexeme, token_type, line_num) in enumerate(reversed(tokens)):
+            if token_type == TokenType.AN:
+                AN_index = len(tokens) - 1 - i
+                break
+        
+        if AN_index == -1:
+            return "NOOB"
+        
+        first_op = tokens[1:AN_index]
+        second_op = tokens[AN_index+1:]
+
+        value1 = get_value(first_op, symbol_table)
+        value2 = get_value(second_op, symbol_table)
+
+        try:
+            operate = operations[operator_type]
+            return operate(value1, value2)
+        except Exception:
+            return ""
+    
+    if operator_type == TokenType.SMOOSH:
+        AN_index = -1
+        for i, (lexeme, token_type, line_num) in enumerate(reversed(tokens)):
+            if token_type == TokenType.AN:
+                AN_index = len(tokens) - 1 - i
+                break
+
+        if AN_index == -1:
+            return "NOOB"
+    
+        first_op = tokens[1:AN_index]
+        second_op = tokens[AN_index+1:]
+
+        value1 = str(get_value(first_op, symbol_table))
+        value2 = str(get_value(second_op, symbol_table))
+        concatval = value1 + value2
+
+        return concatval
+
+    return get_value([tokens[0]], symbol_table)
 
 def symbolize(tokens):
     symbol_table = {}
-    symbol_table['IT'] = ""
+    symbol_table['IT'] = "NOOB"
 
     i = 0
     while i < len(tokens):
@@ -50,13 +132,36 @@ def symbolize(tokens):
             
             var_name = tokens[i + 1][0]
             if var_name not in symbol_table:
-                symbol_table[var_name] = ""
+                symbol_table[var_name] = "NOOB"
             
             index = i + 2
             while index < len(tokens) and tokens[index][1] != TokenType.LINEBREAK:
                 index += 1
 
-            i += index
+            i = index
+            continue
+
+        # reassigning var
+        elif ((token_type == TokenType.VARIDENT) and
+              (i + 2 < len(tokens)) and 
+              (tokens[i + 1][1] == TokenType.R)):
+            
+            var_name = lexeme
+            
+            value_tokens = []
+            index = i + 2
+            while index < len(tokens) and tokens[index][1] != TokenType.LINEBREAK:
+                value_tokens.append(tokens[index])
+                index += 1
+
+            value = get_value(value_tokens, symbol_table)
+
+            if var_name in symbol_table:
+                symbol_table[var_name] = value
+            else:
+                symbol_table[var_name] = value
+
+            i = index
             continue
 
         # VISIBLE
@@ -68,6 +173,12 @@ def symbolize(tokens):
                 index += 1
             
             value = get_value(value_tokens, symbol_table)
+
+            if value == True:
+                value = "WIN"
+            if value == False:
+                value = "FAIL"
+
             symbol_table["IT"] = value
 
             i = index
