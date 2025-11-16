203743c235d5327b39061576793f806ed808c71a
PaulFababeir <pffababeir@up.edu.ph>
2025-11-16 12:37:42 +0800

    feat: implemented control flows, functions, and conditions

--- lolcode_interpreter/semantics/interpreter.py
+++ lolcode_interpreter/semantics/interpreter.py
@@ -3,6 +3,16 @@ from parser.ast_nodes import *
 from lexer.lol_tokens import TokenType
 from semantics import bool_convert
 
+class InterpreterRuntimeError(Exception):
+    pass
+
+class BreakSignal(Exception):
+    pass
+
+class ReturnSignal(Exception):
+    def __init__(self, value):
+        self.value = value
+
 def lol_to_num(value):
     if value == "WIN":
         return 1
@@ -78,9 +88,9 @@ def execute_statement(node, symbol_table, function_table, gui_print, gui_input):
             last_value = evaluate_expression(expression, symbol_table, function_table, gui_print, gui_input)
             outputs.append(lol_to_str(last_value))
 
-        output_string = "".join(outputs)
+        output_string = "".join(outputs) # smoosh
         gui_print(output_string + "\n") # for testing
-        symbol_table["IT"] = last_value    
+        symbol_table["IT"] = last_value #shows the last value of the exp
 
     elif isinstance(node, GimmehNode): # GIMMEH
         if node.var_name not in symbol_table:
@@ -90,6 +100,82 @@ def execute_statement(node, symbol_table, function_table, gui_print, gui_input):
         if user_input is None:
             user_input = "NOOB" #default value
         symbol_table[node.var_name] = user_input
+
+    elif isinstance(node, ConditionalNode):
+        conditional_value = symbol_table.get("IT", "NOOB")
+
+        if bool_convert(conditional_value):
+            for statement in node.if_block:
+                execute_statement(statement, symbol_table, function_table, gui_print, gui_input)
+        
+        else:
+            executed_else_if = False
+            for else_if_clause in node.elif_blocks:
+                else_if_value = evaluate_expression(else_if_clause.condition, symbol_table, function_table, gui_print, gui_input)
+                if bool_convert(else_if_value):
+                    for statement in else_if_clause.statements:
+                        execute_statement(statement, symbol_table, function_table, gui_print, gui_input)
+                    executed_else_if = True
+                    break
+            
+            if not executed_else_if:
+                for statement in node.else_block:
+                    execute_statement(statement, symbol_table, function_table, gui_print, gui_input)
+    
+    elif isinstance(node, SwitchNode):
+        switch_value = lol_to_str(symbol_table.get("IT", "NOOB"))
+        executed_case = False
+
+        try:
+            for case in node.cases:
+                if lol_to_str(case.literal_value) == switch_value:
+                    for statement  in case.statements:
+                        execute_statement(statement, symbol_table, function_table, gui_print, gui_input)
+                    executed_case = True
+                    break
+            
+            if not executed_case:
+                for statement in node.default_case:
+                    execute_statement(statement, symbol_table, function_table, gui_print, gui_input)
+        except BreakSignal:
+            pass
+
+    elif isinstance(node, LoopNode):
+        if node.var_name not in symbol_table:
+            pass
+
+        try:
+            while True:
+                if node.condition:
+                    conditional_value = evaluate_expression(node.condition, symbol_table, function_table, gui_print, gui_input)
+                    if node.condition_type == "WILE":
+                        if not bool_convert(conditional_value):
+                            break
+                    elif node.condition_type == "TIL":
+                        if bool_convert(conditional_value):
+                            break
+                
+                for statement in node.statements:
+                    execute_statement(statement, symbol_table, function_table, gui_print, gui_input)
+
+                current_value = lol_to_num(symbol_table[node.var_name])
+                if node.operation == "UPPIN":
+                    symbol_table[node.var_name] = current_value + 1
+                elif node.operation == "NERFIN":
+                    symbol_table[node.var_name] = current_value - 1
+        except BreakSignal:
+            pass
+    
+    elif isinstance(node, BreakNode):
+        raise BreakSignal()
+
+    elif isinstance(node, ReturnNode):
+        return_value = evaluate_expression(node.expression, symbol_table, function_table, gui_print, gui_input)
+        raise ReturnSignal(return_value)
+    
+    elif isinstance(node, FunctionCallNode):
+        result = evaluate_expression(node, symbol_table, function_table, gui_print, gui_input)
+        symbol_table["IT"] = result
     
     else:
         val = evaluate_expression(node, symbol_table, function_table, gui_print, gui_input)
@@ -171,6 +257,44 @@ def evaluate_expression(node, symbol_table, function_table, gui_print, gui_input
         if node.operator == "ANY OF":
             result = any(bool_convert(op) for op in operands)
             return format_result(result)
+        
+    elif isinstance(node, FunctionCallNode):
+        function_definition = function_table.get(node.func_name)
+        if not function_definition or len(node.arguments) != len(function_definition.parameters):
+            pass
+        
+        argument_values = [evaluate_expression(argument, symbol_table, function_table, gui_print, gui_input) for argument in node.arguments]
+
+        local_symbtable = {"IT": symbol_table.get("IT", "NOOB")} # define its own symbol table
+        for parameters, argument_val in zip(function_definition.parameters, argument_values):
+            local_symbtable[parameters] = argument_val
+
+        try:
+            for statement in function_definition.statements:
+                execute_statement(statement, local_symbtable, function_table, gui_print, gui_input)
+        except ReturnSignal as ret:
+            return ret.value
+    
+        return "NOOB"
+
+    elif isinstance(node, TypecastNode):
+        value = evaluate_expression(node.expression, symbol_table, function_table, gui_print, gui_input)
+        target = node.target_type
+
+        if target == "NUMBR":
+            return int(lol_to_num(value))
+        elif target == "NUMBAR":
+            return float(lol_to_num(value))
+        elif target == "YARN":
+            return lol_to_str(val)
+        elif target == "TROOF":
+            if bool_convert(value):
+                return "WIN"
+            else:
+                return "FAIL"
+        elif target == "NOOB":
+            return "NOOB"
+
     else:
         pass
         # print("Error")
